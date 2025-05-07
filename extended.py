f855ce25a824af21e79e1e6c9e42c887ae1fdc9e
chloee <rebosa.257085@laspinas.sti.edu.ph>
2025-05-08 01:41:25 +0800

    Update extended.py

--- extended.py
+++ extended.py
@@ -290,6 +290,25 @@ class NodeVisitor(object):
         
     def generic_visit(self, node):
         raise Exception('No visit_{} method'.format(type(node).__name__))
+
+    def visit_Compound(self, node):
+        for child in node.children:
+            self.visit(child)
+
+    def visit_NoOp(self, node):
+        pass
+
+    def visit_Assign(self, node):
+        var_name = node.left.value
+        self.GLOBAL_SCOPE[var_name] = self.visit(node.right)
+
+    def visit_Var(self, node):
+        var_name = node.value
+        val = self.GLOBAL_SCOPE.get(var_name)
+        if val is None:
+            raise NameError(repr(var_name))
+        else:
+            return val
         
 class Interpreter(NodeVisitor):
     def __init__(self, parser):
